d009518e8e2e66d9f97cd46eded4d2d36a0c9d2f
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2026-09-19 00:59:25 +0000

    Update: Reshape Matrix

--- problems/0003-reshape-matrix/solution.py
+++ problems/0003-reshape-matrix/solution.py
@@ -2,15 +2,24 @@ import numpy as np
 
 def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
 	#Write your code here and return a python list after reshaping by using numpy's tolist() method
-	result = [[] for _ in range(new_shape[0])]
-	m, n = len(a), len(a[0])
+
+	#### Way 1
+	# result = [[] for _ in range(new_shape[0])]
+	# m, n = len(a), len(a[0])
 	
-	if m * n != new_shape[0] * new_shape[1]:
+	# if m * n != new_shape[0] * new_shape[1]:
+	# 	return []
+
+	# for i in range(new_shape[0]):
+	# 	for j in range(new_shape[1]):
+	# 		pos = i * new_shape[1] + j
+	# 		result[i].append(a[pos // n][pos % n])
+
+	# return result
+
+	#### Way 2
+	try:
+		return np.array(a).reshape(new_shape).tolist()
+	except:
 		return []
-
-	for i in range(new_shape[0]):
-		for j in range(new_shape[1]):
-			pos = i * new_shape[1] + j
-			result[i].append(a[pos // n][pos % n])
-
-	return result
+	# return result.tolist() if result.all != [] else []
