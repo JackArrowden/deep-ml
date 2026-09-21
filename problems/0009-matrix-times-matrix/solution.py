b6e5a48d9b1d8c1fee1aa0a69e490a84fbc28b69
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2026-09-21 09:22:29 +0000

    Update: Matrix times Matrix

--- problems/0009-matrix-times-matrix/solution.py
+++ problems/0009-matrix-times-matrix/solution.py
@@ -1,11 +1,11 @@
 def matrixmul(a:list[list[int|float]],
               b:list[list[int|float]])-> list[list[int|float]]:
-    m, mid, n = len(a), len(a[0]), len(b[0])
+    m, mid, n, vec_b = len(a), len(a[0]), len(b[0]), list(zip(*b))
     if mid != len(b):
         return -1
 
     result = [[] for _ in range(m)]
     for it in range(m * n):
-        result[it // n].append(sum(x * y for x, y in zip(a[it // n], list(zip(*b))[it % n])))
+        result[it // n].append(sum(x * y for x, y in zip(a[it // n], vec_b[it % n])))
         
     return result
