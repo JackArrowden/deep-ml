a22b2b45f184733c6983547040f9c3537f793722
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2026-09-21 12:05:41 +0000

    Update: Calculate Covariance Matrix

--- problems/0010-calculate-covariance-matrix/solution.py
+++ problems/0010-calculate-covariance-matrix/solution.py
@@ -1,10 +1,3 @@
-def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
-	# Your code here
-	n = len(vectors[0])
-	temp = [[(item - sum(vec) / n) for item in vec] for vec in vectors]
-	temp = matrixmul(temp, list(zip(*temp)))
-	return [[item / (n - 1) for item in vec] for vec in temp]
-
 def matrixmul(a:list[list[int|float]],
               b:list[list[int|float]])-> list[list[int|float]]:
     m, mid, n, vec_b = len(a), len(a[0]), len(b[0]), list(zip(*b))
@@ -16,3 +9,8 @@ def matrixmul(a:list[list[int|float]],
         result[it // n].append(sum(x * y for x, y in zip(a[it // n], vec_b[it % n])))
         
     return result
+
+def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
+	n = len(vectors[0])
+	var = [[(item - sum(vec) / n) for item in vec] for vec in vectors]
+	return [[item / (n - 1) for item in vec] for vec in matrixmul(var, list(zip(*var)))]
